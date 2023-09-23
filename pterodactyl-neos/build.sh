07094adff660d7cde7fd0abe4af8c696e362f9ea
bredo <bredo@bredo.tech>
2023-09-23 21:47:02 +1200

    update build

--- pterodactyl-neos/build.sh
+++ pterodactyl-neos/build.sh
@@ -4,4 +4,4 @@ export IMAGE_NAME="local.voxelbone.cloud/neos-pri"
 
 docker buildx build --pull -t $IMAGE_NAME .
 
-#docker save $IMAGE_NAME | bzip2 | pv | ssh vb-us01-dockerbuild docker load
+#docker save $IMAGE_NAME | bzip2 | pv | ssh HOST_HERE docker load
